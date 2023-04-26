de461599b101dcb656504d7bbbe629b0eee8db7f
Heorhii Mykhailenko <georgy_vasilievich@ukr.net>
2023-04-26 15:22:01 +0300

    Make Lection 2 Task 1 validate input

--- Lection_2/Task_1/main.py
+++ Lection_2/Task_1/main.py
@@ -1,9 +1,15 @@
+import sys
+
 list = []
 
-number = int(input("How many elements do you want to enter? "))
+try:
+    number = int(input("How many elements do you want to enter? "))
+except ValueError:
+    print("A number is required.")
+    sys.exit(1)
 
 for element in range(number):
-	list.append(input("Element {}: ".format(element)))
+    list.append(input("Element {}: ".format(element)))
 
 search = input("Enter the element you want to search for: ")
 
