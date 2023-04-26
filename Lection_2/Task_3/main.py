22388ee9d59cecc4464c79de696a7e1187520840
Heorhii Mykhailenko <georgy_vasilievich@ukr.net>
2023-04-26 15:36:03 +0300

    Made Lection 2 Task 3 use sets

--- Lection_2/Task_3/main.py
+++ Lection_2/Task_3/main.py
@@ -1,16 +1,29 @@
+import sys
+
 size = 3
+
+sets = []
+
+setsCombined = []
 
 elements = set()
 
-for listNumber in range(size):
-    list = []
-
-    number = int(input("How many elements do you want to enter in the list {}? ".format(listNumber)))
+for setNumber in range(size):
+    sets.append(set())
+    try:
+        number = int(input("How many elements do you want to enter in the set {}? ".format(setNumber)))
+    except ValueError:
+        sys.exit("A number is required.")
 
     for element in range(number):
         value = input("Element {}: ".format(element))
-        list.append(value)
-        if list.count(value) > 1:
-            elements.add(value)
+        sets[setNumber].add(value)
+        
+    setsCombined += list(sets[setNumber])
+    
+    
+for element in setsCombined:
+    if setsCombined.count(element) > 1:
+        elements.add(element)
 
 print("Non-unique elements:", elements)
